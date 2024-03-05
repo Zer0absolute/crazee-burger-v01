f38ea27b405644d7274d0891e4c975d3c736065b
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-05 15:05:37 +0100

    :lipstick: F03: LOGIN > LoginPage > Add Background and center LoginPage

--- src/component/pages/LOGIN/LoginPage.jsx
+++ src/component/pages/LOGIN/LoginPage.jsx
@@ -1,7 +1,21 @@
 import {LoginForm} from "./LoginForm.jsx";
+import styled from "styled-components";
 
 export const LoginPage = () => {
-    return (
+    return <LoginPageStyled>
         <LoginForm/>
-    )
+    </LoginPageStyled>
 }
+
+const LoginPageStyled = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("/images/F03-burger-background.jpg");
+    background-size: cover;
+    background-position: center;
+    
+`
