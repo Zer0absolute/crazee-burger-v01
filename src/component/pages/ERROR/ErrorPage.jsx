74ded47c825d4246f69af73423111e13d69ba2f0
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-05 14:17:18 +0100

    :construction: F02: ERROR > ErrorPage > Added a 'ErrorPage' heading and a 'Retourner à la page d'accueil' button

--- src/component/pages/ERROR/ErrorPage.jsx
+++ src/component/pages/ERROR/ErrorPage.jsx
@@ -1,5 +1,11 @@
+import {Link} from "react-router-dom";
+
 export const ErrorPage = () => {
-    return (
-        <></>
-    )
+    return <div>
+        <h1>ErrorPage</h1>
+        <br/>
+        <Link to={'/'}>
+            <button>Retourner vers la page d'accueil</button>
+        </Link>
+    </div>
 }
