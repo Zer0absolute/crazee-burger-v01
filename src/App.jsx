ae30abc635bb32381698fcee83fb319d17398425
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-05 13:11:45 +0100

    :construction: F01: App > Create title and form

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+  return (<div>
+    <h1>Bienvenue chez nous !</h1>
+    <br/>
+    <h2>Connectez-vous</h2>
+    <form>
+      <input type={"text"} placeholder={"Entrez votre prénom..."} required/>
+      <button>
+        Accéder à votre espace
+      </button>
+    </form>
+  </div>)
 }
 
 export default App
