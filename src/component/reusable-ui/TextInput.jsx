19bb16c5e185496db1fc5d4e181f79f789e343f9
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-05 20:01:34 +0100

    :construction: F03: LOGIN > LoginForm > Create layout TextInput.jsx

--- src/component/reusable-ui/TextInput.jsx
+++ src/component/reusable-ui/TextInput.jsx
@@ -1,12 +1,47 @@
-export const TextInput = ({ value, onChange, placeholder }) => {
-    return <div>
+// eslint-disable-next-line react/prop-types
+import styled from "styled-components";
+import {theme} from "../../theme/index.jsx";
+
+// eslint-disable-next-line react/prop-types
+export const TextInput = ({Icon, onChange, ...extraProps }) => {
+    return <TextInputStyled>
+        <div className="icon">
+            {Icon && Icon}
+        </div>
         <input
             type={"text"}
-            value={value}
             onChange={onChange}
-            placeholder={placeholder}
-            required
+            {...extraProps}
         />
-    </div>
+    </TextInputStyled>
 }
 
+const TextInputStyled = styled.div`
+    display: flex;
+    width: 400px;
+    
+    .icon {
+        position: absolute;
+        margin: 20px 20px 0 20px;
+        color: ${theme.colors.greySemiDark};
+    }
+    
+    input {
+        display: flex;
+        height: 55px;
+        width: 100%;
+        background: ${theme.colors.white};
+        padding-left: 50px;
+
+        border-radius: ${theme.borderRadius.round};
+        border: none;
+
+        &::placeholder {
+            color: ${theme.colors.greyMedium};
+        }
+
+        &:focus-visible {
+            outline: none;
+        }
+    }
+`
