3112f2ccc5e83ee5a3db7a0ecd4527593da79080
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-05 14:12:07 +0100

    :construction: F02: ORDER > OrderPage > retrieving username to OrderPage

--- src/component/pages/ORDER/OrderPage.jsx
+++ src/component/pages/ORDER/OrderPage.jsx
@@ -1,8 +1,9 @@
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 export const OrderPage = () => {
+    const { username } = useParams()
     return <div>
-        <h1>Bonjour</h1>
+        <h1>Bonjour {username}</h1>
         <br/>
         <Link to={"/"}>
             <button>Déconnexion</button>
