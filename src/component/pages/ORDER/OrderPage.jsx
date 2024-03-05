f448ea276622fb01e275879503e12465b38162c9
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-05 14:03:47 +0100

    :construction: F02: ORDER > OrderPage > Added a 'Bonjour' heading and a 'Déconnexion' button

--- src/component/pages/ORDER/OrderPage.jsx
+++ src/component/pages/ORDER/OrderPage.jsx
@@ -1,5 +1,11 @@
+import {Link} from "react-router-dom";
+
 export const OrderPage = () => {
-    return (
-        <></>
-    )
+    return <div>
+        <h1>Bonjour</h1>
+        <br/>
+        <Link to={"/"}>
+            <button>Déconnexion</button>
+        </Link>
+    </div>
 }
