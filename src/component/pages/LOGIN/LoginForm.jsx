e5d7497e03128674e13bf696fab55452e76e103a
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-05 13:29:09 +0100

    :recycle: F01: LoginForm > Implemented handleSubmit and handleChange functions for form handling

--- src/component/pages/LOGIN/LoginForm.jsx
+++ src/component/pages/LOGIN/LoginForm.jsx
@@ -1,9 +1,21 @@
+import {useState} from "react";
+
 export const LoginForm = () => {
-    return <form>
+    const [inputValue, setInputValue] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert(`Bonjour ${inputValue}`)
+        setInputValue('')
+    }
+    const handleChange = (event) => {
+      setInputValue(event.target.value)
+    }
+
+    return <form action={'submit'} onSubmit={handleSubmit}>
             <h1>Bienvenue chez nous !</h1>
             <br/>
             <h2>Connectez-vous</h2>
-                <input type={"text"} placeholder={"Entrez votre prénom..."} required/>
+                <input type={"text"} value={inputValue} onChange={handleChange} placeholder={"Entrez votre prénom..."} required/>
                 <button>
                     Accéder à votre espace
                 </button>
