bc9b7682fc1ba9f3a41b3279c54eca16f8f539f5
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-06 18:00:52 +0100

    :lipstick: F04: ORDER > Navbar > Profile > Add style

--- src/component/pages/ORDER/Navbar/Navbar.jsx
+++ src/component/pages/ORDER/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
 import styled from "styled-components";
 import {theme} from "../../../../theme/index.jsx";
 import {Logo} from "../../../reusable-ui/Logo.jsx";
+import {Profile} from "./Profile.jsx";
 
 export const Navbar = () => {
     return (
         <NavbarStyled className="navbar">
-            <Logo />
+            <Logo/>
+            <Profile />
         </NavbarStyled>
     )
 }
 
 const NavbarStyled = styled.div`
     display: flex;
+    justify-content: space-between;
     height: 10vh;
     background: ${theme.colors.white};
     border-radius: 15px 15px 0 0;
     padding-left: 20px;
+    font-family: "Open Sans", sans-serif;
 `
