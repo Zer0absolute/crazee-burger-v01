c170cb5bf387e3eb6cedfc12fe270d240d37e412
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-06 17:22:15 +0100

    :sparkles: F04: ORDER > Navbar > Create Profile

---
+++ src/component/pages/ORDER/Navbar/Profile.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Profile = () => {
+    return (
+        <ProfileStyled>
+
+        </ProfileStyled>
+    )
+}
+
+const ProfileStyled = styled.div`
+`
