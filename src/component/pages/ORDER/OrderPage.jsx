e967ece7eed1e149f1a58b9a3cd2b7a5a5bcd7e8
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-06 16:34:59 +0100

    :construction: F04 : ORDER > Add layout

--- src/component/pages/ORDER/OrderPage.jsx
+++ src/component/pages/ORDER/OrderPage.jsx
@@ -1,12 +1,37 @@
 import {Link, useParams} from "react-router-dom";
+import styled from "styled-components";
 
 export const OrderPage = () => {
     const { username } = useParams()
-    return <div>
-        <h1>Bonjour {username}</h1>
-        <br/>
-        <Link to={"/"}>
-            <button>Déconnexion</button>
-        </Link>
-    </div>
+    return <OrderPageStyled>
+        <div className="container">
+            <div className="navbar">
+                navbar
+            </div>
+            {/*<h1>Bonjour {username}</h1>*/}
+            {/*<br/>*/}
+            {/*<Link to={"/"}>*/}
+            {/*    <button>Déconnexion</button>*/}
+            {/*</Link>*/}
+        </div>
+    </OrderPageStyled>
 }
+
+const OrderPageStyled = styled.div`
+    background: orange;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    .container {
+        background: red;
+        height: 95vh;
+        width: 1400px;
+        
+        .navbar {
+            height: 10vh;
+            background: blue;
+        }
+    }
+`
