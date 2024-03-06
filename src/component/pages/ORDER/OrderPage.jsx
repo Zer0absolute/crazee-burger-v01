710d3c48c17e56f7fbbd2655e5192128c8d04f2d
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-06 18:50:18 +0100

    :lipstick: F04: ORDER > Change color background => primary

--- src/component/pages/ORDER/OrderPage.jsx
+++ src/component/pages/ORDER/OrderPage.jsx
@@ -15,7 +15,7 @@ export const OrderPage = () => {
 }
 
 const OrderPageStyled = styled.div`
-    background: orange;
+    background: ${theme.colors.primary};
     height: 100vh;
     display: flex;
     justify-content: center;
