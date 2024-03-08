7f5ccb5978f554b206e094ac35e1833717991dd3
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-08 16:25:10 +0100

    :lipstick: F05: Add grid for Basket and MainRightSide

--- src/components/pages/order/main/Main.jsx
+++ src/components/pages/order/main/Main.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import {theme} from "../../../../theme/index.jsx";
+import {MainRightSide} from "./MainRightSide/MainRightSide.jsx";
 
 export const Main = () => {
     return (
         <MainStyled>
-
+            {/*<div className="basket">*/}
+            {/*    basket*/}
+            {/*</div>*/}
+            <MainRightSide/>
         </MainStyled>
     )
 }
@@ -17,4 +21,12 @@ const MainStyled = styled.main`
     box-shadow: ${theme.shadows.strong};
     
     overflow: hidden;
+    display: grid;
+    grid-template-columns: 1fr;
+    /* This code will be use for Basket */
+    //grid-template-columns: 25% 1fr; 
+    
+    //.basket {
+    //    background: blue;
+    //}
 `
