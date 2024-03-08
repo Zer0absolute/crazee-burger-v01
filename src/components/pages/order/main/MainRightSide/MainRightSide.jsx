8eecb70030dc9d3385f2009e1655e93771b2105d
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-08 16:23:14 +0100

    :sparkles: F05: Create MainRightSide.jsx

---
+++ src/components/pages/order/main/MainRightSide/MainRightSide.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainRightSide = () => {
+    return (
+        <MainRightSideStyled>
+            MainRightSide
+        </MainRightSideStyled>
+    )
+}
+
+const MainRightSideStyled = styled.div`
+    background: red;
+`
