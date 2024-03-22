ce1d0023bf6fecb7105084b5f78df532487b2768
Zer0 <Zer0_AbS0luT@proton.me>
2024-03-22 03:10:31 +0100

    ✨ F07: ORDER > Admin > Create AdminPanel .jsx

--- src/components/pages/order/main/MainRightSide/admin/Admin.jsx
+++ src/components/pages/order/main/MainRightSide/admin/Admin.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-
+import AdminPanel from "./AdminPanel.jsx";
 export default function Admin() {
     return (
         <AdminStyled>
-            Admin
+            <AdminPanel />
         </AdminStyled>
     )
 }
---
+++ src/components/pages/order/main/MainRightSide/admin/AdminPanel.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function AdminPanel() {
+    return (
+        <AdminPanelStyled>
+            AdminPanel
+        </AdminPanelStyled>
+    )
+}
+
+const AdminPanelStyled = styled.div`
+
+`;
