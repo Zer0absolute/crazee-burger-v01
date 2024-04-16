2f99b73a88dba533fc75e5259d4c73fb96575611
Zer0 <Zer0_AbS0luT@proton.me>
2024-04-16 18:21:49 +0200

    💄 F08: MENU > Card > Add style delete button

--- src/components/pages/order/main/MainRightSide/menu/card/Card.jsx
+++ src/components/pages/order/main/MainRightSide/menu/card/Card.jsx
@@ -2,10 +2,16 @@ import styled from "styled-components";
 import {formatPrice} from "../../../../../../../utils/maths.js";
 import {theme} from "../../../../../../../theme/index.jsx";
 import {Button} from "../../../../../../reusable-ui/Button.jsx";
+import { TiDelete } from "react-icons/ti";
 
 export const Card = ({imageSource, title, price}) => {
     return (
         <CardStyled>
+            <div className="delete-button">
+                <button aria-label="delete-button">
+                    <TiDelete />
+                </button>
+            </div>
             <div className="image">
                 <img src={imageSource} alt={title}/>
             </div>
@@ -36,6 +42,26 @@ const CardStyled = styled.div`
     box-shadow: ${theme.shadows.medium};
     border-radius: ${theme.borderRadius.extraRound};
 
+    .delete-button {
+        font-size: 30px;
+        position: absolute;
+        top: 15px;
+        left: 195px;
+        
+        button {
+            border: none;
+            cursor: pointer;
+            font-size: 30px;
+            background: none;
+            color: ${theme.colors.primary};
+
+            &:hover {
+                color: ${theme.colors.red};
+                transition: color 0.3s ease;
+            }
+        }
+    }
+    
     .image {
         width: 200px;
         height: 145px;
