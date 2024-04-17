4d93bc36333dfd4b85feaf9a8c1110a459abf55d
Zer0 <Zer0_AbS0luT@proton.me>
2024-04-17 16:06:14 +0200

    💄 F08: MENU > Add style for EmptyMenuAdmin

--- src/components/pages/order/main/MainRightSide/menu/EmptyMenuAdmin.jsx
+++ src/components/pages/order/main/MainRightSide/menu/EmptyMenuAdmin.jsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+import { theme } from "../../../../../../theme";
+import { Button } from "../../../../../reusable-ui/Button";
+
 export default function EmptyMenuAdmin() {
+
     return (
-        <div>EmptyMenuAdmin</div>
+        <EmptyMenuAdminStyled>
+            <p className="title">Le menu est vide ?</p>
+            <p>Cliquez ci-dessous pour le réinitialiser</p>
+            <div className="button-reset">
+                <Button
+                    label={"Générer de nouveaux produits"}
+                />
+            </div>
+        </EmptyMenuAdminStyled>
     )
 }
+
+const EmptyMenuAdminStyled = styled.div`
+    font-size: ${theme.fonts.size.P4};
+    font-family: Amatic SC, cursive;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .title {
+        font-weight: ${theme.fonts.weights.bold};
+    }
+
+    p {
+        margin-top: -20px;
+        color: ${theme.colors.greyBlue};
+    }
+
+    .button-reset{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        button {
+            width: 225px;
+        }
+    }
+`;
