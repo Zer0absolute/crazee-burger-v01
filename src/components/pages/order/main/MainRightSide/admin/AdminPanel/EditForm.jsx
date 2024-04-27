03f5b65a472fa100e947db69d684b8ed7a606e97
Zer0 <Zer0_AbS0luT@proton.me>
2024-04-27 13:15:55 +0200

    💄 F09: ADMIN > EditForm > Retrieve info of Card clicked on with form from AddForm

--- src/components/pages/order/main/MainRightSide/admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/main/MainRightSide/admin/AdminPanel/EditForm.jsx
@@ -1,14 +1,93 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import HintMessage from "./HintMessage";
 import OrderContext from "../../../../../../../context/OrderContext";
+import styled from "styled-components";
+import ImagePreview from "./ImagePreview";
+import { TextInput } from "../../../../../../reusable-ui/TextInput";
+import { getInputTextsConfig } from "./inputTextConfig";
+import { theme } from "../../../../../../../theme";
+import { EMPTY_PRODUCT } from "../../../../../../../enums/product"
 
 export default function EditForm() {
-    const { productSelected } = useContext(OrderContext)
+    const { productSelected  } = useContext(OrderContext)
+    const [productBeingEdited, setProductBeingEdited] = useState(EMPTY_PRODUCT)
+
+    const inputTexts = getInputTextsConfig(productSelected)
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setProductBeingEdited({
+            ...productBeingEdited,
+            [name] : value,
+        })
+    }
 
     return (
-        <div>
-            <HintMessage />
-            <span>{productSelected && productSelected.title}</span>
-        </div>
+        <EditFormStyled>
+            <ImagePreview imageSource={productSelected.imageSource} title={productSelected.title} />
+            <div className="input-fields">
+                {inputTexts.map((input) => <TextInput 
+                        key={input.id}
+                        onChange={handleChange}
+                        {...input}
+                    />
+                )}
+            </div>
+            <div className="submit">
+                <HintMessage />
+                <span>{productSelected && productSelected.title}</span>
+            </div>
+        </EditFormStyled>
     )
 }
+
+const EditFormStyled = styled.form`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: repeat(4, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 8px;
+    height: 100%;
+    width: 70%;
+
+    .input-fields { 
+        grid-area: 1 / 2 / -4 / 3;
+        display: grid;
+        grid-row-gap: 8px;
+    }
+
+    .submit {
+        grid-area: 4 / -2 / -1 / -1;
+        display: flex;
+        align-items: center;
+        position: relative;
+        top: 3px;
+
+        .submit-button {
+            width: 50%;
+        }
+
+        .submit-message {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-left: 5px;
+
+            .icon {
+                color: ${theme.colors.success};
+                margin-left: 10px;
+                width: 1em;
+                height: 1em;
+                border: 1px solid ${theme.colors.success};
+                border-radius: 50%;
+                vertical-align: middle;
+            }
+            .message {
+                margin-left: 5px;
+                font-size: ${theme.fonts.size.SM};
+                color: ${theme.colors.success};
+                font-family: ${theme.fonts.family.globalText};
+            }
+        }
+    }
+`;
