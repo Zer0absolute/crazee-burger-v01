c3d9b3669cedcf0969fa829a521c1dccb0349a68
Zer0 <Zer0_AbS0luT@proton.me>
2024-05-10 16:20:56 +0200

    ♻️ F10> BASKET > Add Total

---
+++ src/components/pages/order/main/Basket/Total.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+import Header from "../../../../reusable-ui/Header";
+
+export default function Total({ amountToPay }) {
+    return (
+        <Header>
+            <TotalStyled>
+                <span>Total</span>
+                <span>{amountToPay}</span>
+            </TotalStyled>
+        </Header>
+    )
+}
+
+const TotalStyled = styled.div`
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: ${theme.colors.primary};
+    font-family: "Amatic SC", cursive;
+    font-size: ${theme.fonts.size.P4};
+    font-weight: ${theme.fonts.weights.regular};
+`;
