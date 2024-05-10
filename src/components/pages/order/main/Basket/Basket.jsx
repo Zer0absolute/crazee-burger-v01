2f72e4bb475b26860dce8c666dfa2bbba993da2d
Zer0 <Zer0_AbS0luT@proton.me>
2024-05-10 16:21:32 +0200

    ♻️ F10> BASKET > Add BasketBody.jsx

--- src/components/pages/order/main/Basket/Basket.jsx
+++ src/components/pages/order/main/Basket/Basket.jsx
@@ -1,20 +1,17 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
 import { formatPrice } from "../../../../../utils/maths";
+import Header from "../../../../reusable-ui/Header";
+import Total from "./Total";
+import BasketBody from "./BasketBody";
+import Footer from "./Footer";
 
 export default function Basket() {
     return (
         <BasketStyled>
-            <div className="total">
-                <span className="text-total">Total</span>
-                <span className="price">{formatPrice(0)}</span>
-            </div>
-            <div className="basket-body">
-                <span className="empty-message">Votre commande est vide.</span>
-            </div>
-            <div className="footer">
-                <span>Codé avec ❤️ et React.JS</span>
-            </div>
+            <Total amountToPay={formatPrice(0)}/>
+            <BasketBody/>
+            <Footer/>
         </BasketStyled>
     )
 }
@@ -22,53 +19,4 @@ export default function Basket() {
 const BasketStyled = styled.div`
     display: flex;
     flex-direction: column;
-
-    .total {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        color: ${theme.colors.primary};
-        font-family: "Amatic SC", cursive;
-        font-size: ${theme.fonts.size.P4};
-        font-weight: ${theme.fonts.weights.regular};
-        letter-spacing: 2px;
-        height: 70px;
-        background: ${theme.colors.background_dark};
-        padding: 0 16px;
-    }
-
-    .basket-body {
-        flex: 1;
-        background: ${theme.colors.background_white};
-        box-shadow: ${theme.shadows.basket};
-
-        .empty-message {
-            display: flex;
-            height: calc(95vh - 10vh - 70px - 70px);
-            text-align: center;
-            flex: 1;
-            justify-content: center;
-            align-items: center;
-            align-self: center;
-            line-height: 2;
-            font-family: ${theme.fonts.family.stylish};
-            font-size: ${theme.fonts.size.P4};
-            color: ${theme.colors.greyBlue};
-        }
-    }
-
-    .footer {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        letter-spacing: 2px;
-        height: 70px;
-        background: ${theme.colors.background_dark};
-        padding: 0 16px;
-        font-size: ${theme.fonts.size.P2};
-        font-family: ${theme.fonts.family.stylish};
-        font-weight: ${theme.fonts.weights.bold};
-        color: ${theme.colors.white};
-        border-bottom-left-radius: ${theme.borderRadius.round};
-    }
 `;
