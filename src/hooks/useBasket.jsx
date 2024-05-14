8a906c2c9a3a0a9087f06f6bae8351afff5250e0
Zer0 <Zer0_AbS0luT@proton.me>
2024-05-14 20:59:14 +0200

    ✨ F11: BASKET > Add Product >Create handleAddToBasket (when product is NOT already in basket)

--- src/hooks/useBasket.jsx
+++ src/hooks/useBasket.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react"
 import { fakeBasket } from "../fakeData/fakeBasket"
+import { deepClone, findToArray } from "../utils/array"
 
 export const useBasket = () => {
     const [basket, setBasket] = useState(fakeBasket.EMPTY)
 
     const handleAddToBasket = (productToAdd) => {
-        return
+        const basketCopy = deepClone(basket)
+        const isProductAlreadyInBasket = findToArray(productToAdd.id, basketCopy)
+        // 1er cas : Le produit n'est pas dans le basket
+        if(!isProductAlreadyInBasket) {
+            const newBasketProduct = {
+                ...productToAdd,
+                quantity: 1,
+            }
+
+            const basketUpdated = [newBasketProduct, ...basketCopy]
+            setBasket(basketUpdated)
+        }
+
+        //2eme cas : le produit est déjà dans le basket
     }
     return { basket, handleAddToBasket }
 }
