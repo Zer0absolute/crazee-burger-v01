385a5503a2186bc3a8a93911d9f07712eafb15a8
Zer0 <Zer0_AbS0luT@proton.me>
2024-05-21 22:31:00 +0200

    🚧 F14: BASKET > Total > Make both elements visible on props change

--- src/components/reusable-ui/CassinoEffect.jsx
+++ src/components/reusable-ui/CassinoEffect.jsx
@@ -1,5 +1,37 @@
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import styled from "styled-components";
+
 export default function CassinoEffect({ count, className }) {
     return (
-        <span className={className}>{count}</span>
+        <TransitionGroup component={CassinoEffectStyled}>
+            <CSSTransition classNames={"count-animeted"} timeout={5000} key={count}>
+                <span className={className}>
+                    {count}
+                </span>
+            </CSSTransition>
+        </TransitionGroup>
     )
 }
+
+const CassinoEffectStyled = styled.div`
+    border: 1px solid red;
+
+    /* MOUNTING */
+    .count-animated-enter {
+    }
+
+    .count-animated-enter-active {
+        transition: 2s;
+    }
+
+    .count-animated-enter-done {
+    }
+
+        /* UNMOUNTING */
+    .count-animated-exit {
+    }
+
+    .count-animated-exit-active {
+        transition: 2s;
+    }
+`;
