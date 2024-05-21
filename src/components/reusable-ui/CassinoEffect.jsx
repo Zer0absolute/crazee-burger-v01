baa63ad1387534397267aea8c3eb4b9a028ca3ac
Zer0 <Zer0_AbS0luT@proton.me>
2024-05-21 22:55:53 +0200

    ✨ F14: BASKET > Total > Add CasinoEffect (finished)

--- src/components/reusable-ui/CassinoEffect.jsx
+++ src/components/reusable-ui/CassinoEffect.jsx
@@ -1,37 +1,51 @@
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import styled from "styled-components";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styled from 'styled-components';
 
-export default function CassinoEffect({ count, className }) {
+export default function CassinoEffect({ count }) {
     return (
         <TransitionGroup component={CassinoEffectStyled}>
-            <CSSTransition classNames={"count-animeted"} timeout={5000} key={count}>
-                <span className={className}>
+            <CSSTransition classNames="count-animated" timeout={300} key={count}>
+                <span>
                     {count}
                 </span>
             </CSSTransition>
         </TransitionGroup>
-    )
+    );
 }
 
 const CassinoEffectStyled = styled.div`
-    border: 1px solid red;
+    /* border: 1px solid red; */
+    position: relative;
+    overflow: hidden;
+
+    span {
+        display: inline-block;
+    }
 
     /* MOUNTING */
     .count-animated-enter {
+        transform: translateY(100%);
     }
 
     .count-animated-enter-active {
-        transition: 2s;
+        transform: translateY(0%);
+        transition: transform 300ms;
     }
 
     .count-animated-enter-done {
+        transform: translateY(0%);
     }
 
-        /* UNMOUNTING */
+    /* UNMOUNTING */
     .count-animated-exit {
+        transform: translateY(0%);
+        position: absolute;
+        bottom: 0;
+        right: 0;
     }
 
     .count-animated-exit-active {
-        transition: 2s;
+        transform: translateY(-100%);
+        transition: transform 300ms;
     }
 `;
